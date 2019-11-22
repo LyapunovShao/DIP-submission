a290e790fdcf40108449a0a238fefca28f71d3bd
LyapunovShao <1159326041@sjtu.edu.cn>
2019-11-22 23:37:30 +0800

    add color to depth image

---
+++ color.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+from PIL import Image
+
+path = './'
+png = 'disparity.png'
+alpha = 2.2
+
+def main():
+    depth = cv2.imread(path+png)
+    
+    color = cv2.applyColorMap(cv2.convertScaleAbs(
+        depth, alpha=alpha), cv2.COLORMAP_HSV)
+    image = Image.fromarray(color)
+    image.save(path+'converted-'+png)
+
+
+if __name__ == '__main__':
+    main()
